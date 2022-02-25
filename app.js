ae57fe446eb73ed8e53574e67433db0af7a72287
Lars <l.gessner@posteo.de>
2022-02-25 14:29:35 +0100

    add post to show username

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express")
 
 const app = express()
 
-
 app.set("view engine", "pug")
 
+app.use(express.urlencoded())
+
+// ROUTES GET
 app.get("/", (req, res) => {
     res.render("index")
 })
@@ -13,6 +15,19 @@ app.get("/cards", (req, res) => {
     res.render("card", {prompt: "red in german", hint: "A big o in the middle"})
 })
 
+
+app.get("/hello", (req, res) => {
+    res.render("hello", req.body.username ? { name: req.body.username } : { name: "anon" } )
+})
+
+
+// ROUTES POST
+app.post("/hello", (req, res) => {
+    res.render("hello", req.body.username ? { name: req.body.username } : { name: "anon" } )
+})
+
+
+// LISTEN
 app.listen(3000, () => {
     console.log("App run on localhost:3000")  
 })
