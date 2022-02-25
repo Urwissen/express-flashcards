ffbc38fb3da467fa28f8df07b946006e5113e0b7
Lars <l.gessner@posteo.de>
2022-02-25 11:55:40 +0100

    add route

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require("express")
 
 const app = express()
 
+
 app.set("view engine", "pug")
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+
+app.get("/cards", (req, res) => {
+    res.render("card", {prompt: "red in german", hint: "A big o in the middle"})
 })
 
 app.listen(3000, () => {
